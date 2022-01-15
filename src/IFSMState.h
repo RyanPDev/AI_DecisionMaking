cc166b28dc7b5c67a40aba7d361adc9fc6651f24
Ryan Palazón <ryan.palazon@enti.cat>
2022-01-15 18:11:25 +0100

    update

--- src/IFSMState.h
+++ src/IFSMState.h
@@ -5,6 +5,8 @@ class Agent;
 class IFSMState
 {
 public:
+	virtual ~IFSMState() {}
+
 	virtual IFSMState* Update(Agent*, float) = 0;
 	virtual void Enter(Agent*, float) = 0;
 	virtual void Exit(Agent*, float) = 0;
